fdfa71dbc971f1fff4011e407adbeeb1d20c5249
James Hagood <cardfaux@tutanota.com>
2020-03-19 15:55:55 -0500

    added App.ts

---
+++ App.ts
@@ -0,0 +1,14 @@
+const button = document.querySelector('button');
+const input1 = document.getElementById('num1')! as HTMLInputElement; // Type Casting
+const input2 = document.getElementById('num2')! as HTMLInputElement; // The ! tells TS it will never be null
+
+// In JavaScript Inputs Are Always Strings, The Value Of Any Input Element Will Always Be A String
+
+function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+button.addEventListener('click', function() {
+	// Here We Converted Both To Numbers With the +
+	console.log(add(+input1.value, +input2.value));
+});
