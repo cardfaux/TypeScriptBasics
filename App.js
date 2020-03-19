629b54d6b507026ae0fe66db2519e2625f4b5b00
James Hagood <cardfaux@tutanota.com>
2020-03-19 18:04:41 -0500

    added package json

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 // const button = document.querySelector('button');
 // const input1 = document.getElementById('num1')! as HTMLInputElement; // Type Casting
 // const input2 = document.getElementById('num2')! as HTMLInputElement; // The ! tells TS it will never be null
-
 // // In JavaScript Inputs Are Always Strings, The Value Of Any Input Element Will Always Be A String
-
 // function add(num1: number, num2: number) {
 // 	return num1 + num2;
 // }
-
 // button.addEventListener('click', function() {
 // 	// Here We Converted Both To Numbers With the +
 // 	console.log(add(+input1.value, +input2.value));
 // });
-
-function add(n1: number, n2: number) {
-	return n1 + n2;
+function add(n1, n2) {
+    return n1 + n2;
 }
-
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
 console.log(result);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TypeScript Basics
+
+## Core Data Types
+
+# number 1, 5.3, -10
+
+# string 'hi', "Hi", `hi`
+
+# boolean true or false
