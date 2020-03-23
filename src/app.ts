97aa0dcc486601aadc2f9146a69dc0f7fee90318
James Hagood <cardfaux@tutanota.com>
2020-03-22 19:23:09 -0500

    added generics

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,46 @@ promise.then((data) => {
 });
 
 // Build A Generic Function
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
 	return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Adam' }, { age: 31 });
 mergedObj.age;
 mergedObj.name;
+
+interface Lengthly {
+	length: number;
+}
+function countAndDescribe<T extends Lengthly>(element: T): [T, string] {
+	let descriptionText = 'Got No Value.';
+	if (element.length > 0) {
+		descriptionText = 'Got ' + element.length + ' elements.';
+	} else if (element.length > 1) {
+		descriptionText = 'Got ' + element.length + ' elements';
+	}
+	return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi There!'));
+console.log(countAndDescribe(['sports', 'cooking']));
+
+// KeyOf Constraints
+function extractAndConvert<T extends object, U extends keyof T>(
+	obj: T,
+	key: U
+) {
+	return obj[key];
+}
+extractAndConvert({ name: 'Adam' }, 'name');
+
+class DataStorage<T> {
+	private data: T[] = [];
+	addItem(item: T) {
+		this.data.push(item);
+	}
+	removeItem(item: T) {
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+	getItems() {
+		return [...this.data];
+	}
+}
